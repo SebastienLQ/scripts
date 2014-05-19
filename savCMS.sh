dfef54d69fc7b5ebcfbded8cef3b7fecc7be780a
sebidouille <sebastien@lequerec.fr>
2014-05-19 23:13:14 +0200

    script de sauvegarde CMS

--- savCMS.sh
+++ savCMS.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 DATE=`date +%Y_%m_%d`
-echo $DATE
-#sudo drush sql-dump > /var/www/blog.lequerec.fr/database/seb.sql
-#sudo tar -zcf ./seblog.tar.gz /var/www/blog.lequerec.fr
-
+echo "Entrer le repertoires des sites : "
+read sitedir
 echo "Entrez le repertoire de sauvegarde :"
 read dir
 mkdir $dir
 echo "Entrez votre login mysql:"
-stty -echo
 read login
-stty echo
 echo "Entrez votre mot de passe mysql:"
 stty -echo
 read passe
@@ -20,9 +16,15 @@ echo $LISTEBDD
 for SQL in $LISTEBDD
 do
 if [ $SQL != "information_schema" ] && [ $SQL != "mysql" ] && [ $SQL != "Database" ] && [ $SQL != "performance_schema" ]; then
-echo $SQL
+echo -e "Compression de :"$SQL"\tvers :\t"$dir/$DATE"_mysql_"$SQL.sql.gz
 mysqldump -$login --password=$passe $SQL | gzip > $dir/$DATE"_mysql_"$SQL.sql.gz
 fi
 done
-
+LISTSITE="$(find $sitedir/* -type d -prune | awk -F"/" '{print $NF}')"
+echo $LISTSITE
+for SITE in $LISTSITE; 
+do 
+echo -e "Compression de : "$SITE"\tvers :\t"$dir/$DATE-$SITE".tar.gz"
+tar -zcf $dir/$DATE-$SITE.tar.gz $SITE
+done
  
