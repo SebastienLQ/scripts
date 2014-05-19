04e5927bde5e0f2f9dbf13eb3a4af49d98784477
sebidouille <sebastien@lequerec.fr>
2014-05-19 19:28:00 +0200

    MAJ sav.sh

--- sav.sh
+++ sav.sh
@@ -3,13 +3,25 @@ DATE=`date +%Y_%m_%d`
 echo $DATE
 #sudo drush sql-dump > /var/www/blog.lequerec.fr/database/seb.sql
 #sudo tar -zcf ./seblog.tar.gz /var/www/blog.lequerec.fr
-LISTEBDD=$( echo 'show databases' | mysql -u root --password=yourpass )
+
+echo "Entrez le repertoire de sauvegarde :"
+read dir
+mkdir $dir
+echo "Entrez votre login mysql:"
+stty -echo
+read login
+stty echo
+echo "Entrez votre mot de passe mysql:"
+stty -echo
+read passe
+stty echo
+LISTEBDD=$( echo 'show databases' | mysql -u $login --password=$passe )
 echo $LISTEBDD
 for SQL in $LISTEBDD
 do
 if [ $SQL != "information_schema" ] && [ $SQL != "mysql" ] && [ $SQL != "Database" ] && [ $SQL != "performance_schema" ]; then
 echo $SQL
-mysqldump -root --password=yourpass $SQL | gzip > ./$SQL"_mysql_"$DATE.sql.gz
+mysqldump -$login --password=$passe $SQL | gzip > $dir/$DATE"_mysql_"$SQL.sql.gz
 fi
 done
 
